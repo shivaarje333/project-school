f22d7d092211c55981dcd1c84fae16e55aa3184c
gulshan0308 <mogulshanqureshi03@gmail.com>
2023-11-12 09:23:07 +0530

    all ok

--- gulshan.js
+++ gulshan.js
@@ -1,6 +1,24 @@
-let signinbtn,signinform;
+let signinbtn,signinform,UserLoginPage,togglesign;
 signinbtn=document.getElementById('signinbtn');
 signinform=document.getElementById('signinform');
+UserLoginPage=document.getElementById('UserLoginPage');
+togglesign = document.querySelectorAll('.toggle-sign');
+
 signinbtn.addEventListener('click',()=>{
      signinform.classList.toggle("d-none")
+})
+
+for(i of togglesign){
+     i.addEventListener('click',()=>{
+          UserLoginPage.classList.toggle("d-none")
+     })
+     
+}
+document.body.addEventListener('click', function(){
+   if(UserLoginPage.style.display = "block"){
+     UserLoginPage.style.display = "none"     
+   }else{
+     UserLoginPage.style.display = "block"
+   }
+   
 })
