177ce7025605691d6f9ec4bc3bdf9a472b566ad2
gulshan0308 <mogulshanqureshi03@gmail.com>
2023-11-13 09:05:41 +0530

    all

--- gulshan.js
+++ gulshan.js
@@ -1,16 +1,27 @@
-let signinbtn,signinform,UserLoginPage,togglesign;
+let signinbtn,signinform,UserLoginPage,togglesign,SignUpFormBtn,signupform,UserLoginPageBtn;
 signinbtn=document.getElementById('signinbtn');
 signinform=document.getElementById('signinform');
 UserLoginPage=document.getElementById('UserLoginPage');
 togglesign = document.querySelectorAll('.toggle-sign');
-
+SignUpFormBtn=document.getElementById('SignUpFormBtn');
+signupform=document.getElementById('signupform');
+UserLoginPageBtn=document.getElementById('UserLoginPageBtn');
+// first nav click btn for sign in users
 signinbtn.addEventListener('click',()=>{
      signinform.classList.toggle("d-none")
 })
-
+// login page for specific type of user
 for(i of togglesign){
      i.addEventListener('click',()=>{
-          UserLoginPage.classList.toggle("d-none")
-     })
-     
+          UserLoginPage.classList.remove("d-none")
+     })   
 }
+
+SignUpFormBtn.addEventListener('click',()=>{
+     signupform.classList.remove('d-none');
+     
+})
+UserLoginPageBtn.addEventListener('click',()=>{
+     UserLoginPage.classList.remove("d-none");
+     signupform.classList.remove('d-block');
+})
